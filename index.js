44ced51b9b3b76ee1d5fec01b619123c28a2510d
shwetanegi <108540488+shwetanegi30@users.noreply.github.com>
2022-09-27 12:35:03 +0530

    changes

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const dailyBtn = document.getElementById("daily-btn");
 const weeklyBtn = document.getElementById("weekly-btn");
 const monthlyBtn = document.getElementById("monthly-btn");
 const CardAll = document.querySelectorAll("card");
-const ellipsisIcon = document.getElementById("ellipsis-icon");
-const cardBody =document.querySelectorAll("card-body");
-let timeHour = document.querySelectorAll("time-hour");
-let text = document.querySelectorAll("text");
+const cardBody =document.getElementById("card-body-title");
+let timeHour = document.getElementById("time-hour");
+let text = document.getElementById("text");
 
-fetch('./data.json', {
-    method: 'GET'
-})
-.then(function(response) {return response.json(); })
-.then(function(json) {
-    console.log(json)
-});
+
+function requestData() {
+  fetch('./data.json')
+   .then(response => response.json())
+   .then((data) => {
+    console.log (data)
+   })
+
 
 dailyBtn.addEventListener("click", (event) => {
   CardAll.forEach((element) => {
@@ -23,7 +23,7 @@ dailyBtn.addEventListener("click", (event) => {
         data[i].timeframes.daily.current +
         (data[i].timeframes.daily.current > 1 ? "hrs" : "hr");
       text[i].textContent =
-        "previous - " +
+        "Last day - " +
         data[i].timeframes.daily.previous +
         (data[i].timeframes.daily.previous > 1 ? "hrs" : "hr");
     }
@@ -31,7 +31,6 @@ dailyBtn.addEventListener("click", (event) => {
   changeActive(dailyBtn, weeklyBtn, monthlyBtn);
   event.preventDefault();
 });
-
 
 weeklyBtn.addEventListener("click", (event) => {
     CardAll.forEach((element) => {
@@ -45,8 +44,8 @@ weeklyBtn.addEventListener("click", (event) => {
     changeActive(weeklyBtn, monthlyBtn, dailyBtn);
     event.preventDefault();
   });
-
-  monthlyBtn.addEventListener("click", (event) => {
+  
+    monthlyBtn.addEventListener("click", (event) => {
     CardAll.forEach((element) => {
 
       for (let i = 0; i < data.length; i++) {
@@ -58,55 +57,134 @@ weeklyBtn.addEventListener("click", (event) => {
     changeActive(monthlyBtn, weeklyBtn, dailyBtn);
     event.preventDefault();
   });
-  
-    requestData();
+
+}
+requestData();
 
     function changeActive(element, previousActive1, previousActive2) {
         previousActive1.classList.remove("active");
         previousActive2.classList.remove("active");
         element.classList.add("active");
-      }
 
-      function extraInfo() {
+    }
 
-        let closedWork = false;
 
-        ellipsisIcon.addEventListener("click", () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ellipsisIcon = document.getElementById("ellipsis-icon");
+// const cardBody =document.querySelectorAll("card-body-title");
+// let timeHour = document.querySelectorAll("time-hour");
+// let text = document.querySelectorAll("text");
+
+// function requestData() {
+//   fetch('./data.json')
+//    .then(response => response.json())
+//    .then((data) => {
+//     console.log (data)
+//    })
+// }
+
+// dailyBtn.addEventListener("click", (event) => {
+//   CardAll.forEach((element) => {
+
+//     for (let i = 0; i < data.length; i++) {
+//       timeHour[i].textContent =
+//         data[i].timeframes.daily.current +
+//         (data[i].timeframes.daily.current > 1 ? "hrs" : "hr");
+//       text[i].textContent =
+//         "previous - " +
+//         data[i].timeframes.daily.previous +
+//         (data[i].timeframes.daily.previous > 1 ? "hrs" : "hr");
+//     }
+//   });
+//   changeActive(dailyBtn, weeklyBtn, monthlyBtn);
+//   event.preventDefault();
+// });
+
+
+// weeklyBtn.addEventListener("click", (event) => {
+//     CardAll.forEach((element) => {
+
+//       for (let i = 0; i < data.length; i++) {
+//         timeHour[i].textContent = data[i].timeframes.weekly.current + "hrs";
+//         text[i].textContent =
+//           "Last Week - " + data[i].timeframes.weekly.previous + "hrs";
+//       }
+//     });
+//     changeActive(weeklyBtn, monthlyBtn, dailyBtn);
+//     event.preventDefault();
+//   });
+
+//   monthlyBtn.addEventListener("click", (event) => {
+//     CardAll.forEach((element) => {
+
+//       for (let i = 0; i < data.length; i++) {
+//         timeHour[i].textContent = data[i].timeframes.monthly.current + "hrs";
+//         text[i].textContent =
+//           "Last Month - " + data[i].timeframes.monthly.previous + "hrs";
+//       }
+//     });
+//     changeActive(monthlyBtn, weeklyBtn, dailyBtn);
+//     event.preventDefault();
+//   });
+  
+//     requestData();
+
+//     function changeActive(element, previousActive1, previousActive2) {
+//         previousActive1.classList.remove("active");
+//         previousActive2.classList.remove("active");
+//         element.classList.add("active");
+//       }
+
+//       function extraInfo() {
+
+//         let closedWork = false;
+
+//         ellipsisIcon.addEventListener("click", () => {
             
-            if (!closedWork) {
-              CardAll.classList.remove("hidden");
-              cardBody[0].classList.remove("border-radius");
-              cardBody[0].classList.add("border-top-radius");
-              closedWork = true;
+//             if (!closedWork) {
+//               CardAll.classList.remove("hidden");
+//               cardBody[0].classList.remove("border-radius");
+//               cardBody[0].classList.add("border-top-radius");
+//               closedWork = true;
               
-            } else {
-              CardAll.classList.add("hidden");
-              cardBody[0].classList.add("border-radius");
-              cardBody[0].classList.remove("border-top-radius");
-              closedWork = false;
-            }
-          });
-        }
-        extraInfo();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//             } else {
+//               CardAll.classList.add("hidden");
+//               cardBody[0].classList.add("border-radius");
+//               cardBody[0].classList.remove("border-top-radius");
+//               closedWork = false;
+//             }
+//           });
+//         }
+//         extraInfo();
 
 
 
